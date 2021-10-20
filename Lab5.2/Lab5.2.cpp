fa55296fb9d28c96103bf0ef796c6f84cd9c52e6
YuliiaDobosh <yuliia.dobosh.it.2021.lpnu.ua>
2021-10-20 10:08:03 +0300

    new

---
+++ Lab5.2/Lab5.2.cpp
@@ -0,0 +1,61 @@
+// Лабораторна робота № 5.2
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+void S(const double x, const double eps, int& n, double& s);
+void A(const double x, const int n, double& a);
+double Pi = 3.14159265359;
+
+int main()
+
+{
+	double xp, xk, x, dx, eps, s = 0;
+	int n = 0;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+	cout << fixed;
+	cout << "-------------------------------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(10) << "arcctg(x)" << " |"
+		<< setw(7) << "S" << " |"
+		<< setw(5) << "n" << " |"
+		<< endl;
+	cout << "-------------------------------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		  S(x, eps, n,s);
+		cout << "|" << setw(7) << setprecision(2) << x << " |"
+			<< setw(10) << setprecision(5) << (Pi/2)-atan(x) << " |"
+			<< setw(10) << setprecision(5) << s << " |"
+			<< setw(5) << n << " |"
+			<< endl;
+		x += dx;
+	}
+	cout << "-------------------------------------------------" << endl;
+	return 0;
+}
+void S(const double x, const double eps, int& n, double& s)
+{
+	n = 0; // вираз залежить від умови завдання варіанту
+	double a = -x; // вираз залежить від умови завдання варіанту
+	s = a;
+	do {
+		n++;
+		A(x, n, a);
+		s += a;
+	} while (abs(a) >= eps);
+	s += Pi / 2;
+}
+
+void A(const double x, const int n, double& a)
+{
+	double R = -((x * x * (2. * n - 1)) / (2. * n + 1));  // вираз залежить від умови завдання варіанту
+	a *= R;
+}
+
+
+
